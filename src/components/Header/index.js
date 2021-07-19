2fba0941c6d87c45cc19c07b181bc3d3c42c7f39
flodelo <florian.delobelle@gmail.com>
2021-07-19 17:42:06 +0200

    Test Chakra UI on Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,15 +4,15 @@ import { Flex, Spacer, Box, Heading, Button } from "@chakra-ui/react";
 const Header = () => (
     <Flex>
     <Box p="2">
-      <Heading size="lg">React-jobs.fr</Heading>
+      <Heading size="lg"><span role="img" aria-label="atom emoji">⚛</span> React-jobs.fr</Heading>
     </Box>
     <Spacer />
     <Box>
-      <Button colorScheme="blue" mr="2">
+      <Button color="blue.500" variant="outline" mr="2">
         Sign Up
       </Button>
-      <Button colorScheme="blue" variant="outline" mr="2">Log in</Button>
-      <Button colorScheme="blue">Menu</Button>
+      <Button color="blue.500" mr="2" >Log in</Button>
+      <Button color="black" variant="ghost">Menu</Button>
     </Box>
   </Flex>
 )
