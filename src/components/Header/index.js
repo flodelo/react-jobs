1a5c5a3fe9f92b1a41e298eba0b76de9082b2b8b
flodelo <florian.delobelle@gmail.com>
2021-07-19 19:10:17 +0200

    Chakra UI test

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Flex, Spacer, Box, Heading, Button } from "@chakra-ui/react";
+import { Flex, Spacer, Box, Heading, Button, useBreakpointValue } from "@chakra-ui/react";
 
-const Header = () => (
+const Header = () => {
+
+  const buttonSize = useBreakpointValue(['xs', 'md', 'lg']);
+
+  return(
     <Flex>
     <Box p="2">
-      <Heading size="lg"><span role="img" aria-label="atom emoji">⚛</span> React-jobs.fr</Heading>
+      <Heading size="xl"><span role="img" aria-label="atom emoji">⚛</span> React-jobs.fr</Heading>
     </Box>
     <Spacer />
     <Box>
-      <Button color="blue.500" variant="outline" mr="2">
+      <Button size={buttonSize} color="blue.500" variant="outline" mr="2">
         Sign Up
       </Button>
-      <Button color="blue.500" mr="2" >Log in</Button>
-      <Button color="black" variant="ghost">Menu</Button>
+      <Button size={buttonSize} color="blue.500" mr="2" >Log in</Button>
+      <Button size={buttonSize} color="black" variant="ghost">Menu</Button>
     </Box>
   </Flex>
-)
+)}
 
 export default Header;
