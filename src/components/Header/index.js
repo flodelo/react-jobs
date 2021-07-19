61535aaf33f6af664b044712ec477f99a7bbf834
flodelo <florian.delobelle@gmail.com>
2021-07-19 19:20:23 +0200

    comments added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,20 +3,21 @@ import { Flex, Spacer, Box, Heading, Button, useBreakpointValue } from "@chakra-
 
 const Header = () => {
 
-  const buttonSize = useBreakpointValue(['xs', 'md', 'lg']);
+  const responsiveSize = useBreakpointValue(['xs', 'md', 'lg']);
+  // Le hook useBreakpointValue répond également au redimensionnement de la fenêtre et renvoie la valeur appropriée pour la nouvelle taille de la fenêtre
 
   return(
     <Flex>
     <Box p="2">
-      <Heading size="xl"><span role="img" aria-label="atom emoji">⚛</span> React-jobs.fr</Heading>
+      <Heading size={responsiveSize}><span role="img" aria-label="atom emoji">⚛</span> React-jobs.fr</Heading>
     </Box>
     <Spacer />
     <Box>
-      <Button size={buttonSize} color="blue.500" variant="outline" mr="2">
+      <Button size={responsiveSize} color="blue.500" variant="outline" mr="2">
         Sign Up
       </Button>
-      <Button size={buttonSize} color="blue.500" mr="2" >Log in</Button>
-      <Button size={buttonSize} color="black" variant="ghost">Menu</Button>
+      <Button size={responsiveSize} color="blue.500" mr="2" >Log in</Button>
+      <Button size={responsiveSize} color="black" variant="ghost">Menu</Button>
     </Box>
   </Flex>
 )}
