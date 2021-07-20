ee7ca86c24db031c11bdfa2a9d74ed184d51dea6
flodelo <florian.delobelle@gmail.com>
2021-07-20 10:40:59 +0200

    eslint errors

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,52 +1,59 @@
+/* eslint-disable max-len */
 import React from 'react';
-import { Flex, Spacer, Box, Heading, Button, IconButton, Menu, MenuButton, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Flex, Spacer, Box, Heading, Button, IconButton, Menu, MenuButton, useBreakpointValue,
+} from '@chakra-ui/react';
 
 import { HamburgerIcon } from '@chakra-ui/icons';
 
 const Header = () => {
-
   const responsiveSize = useBreakpointValue(['xs', 'md', 'lg']);
   // Le hook useBreakpointValue répond également au redimensionnement de la fenêtre et renvoie la valeur appropriée pour la nouvelle taille de la fenêtre
 
-  return(
+  return (
     <Flex>
-    <Box p="2">
-      <Heading size={responsiveSize}><span role="img" aria-label="atom emoji">⚛</span> React-jobs.fr</Heading>
-    </Box>
-    <Spacer />
-    <Box>
-      <Button 
-      display={{
-        sm: "none",
-        md: "inline-block",
-        lg: "inline-block",
-      }}
-      size={responsiveSize} 
-      color="blue.500" 
-      variant="outline" mr="2">
-        Sign Up
-      </Button>
-      <Button 
-      display={{
-        sm: "none",
-        md: "inline-block",
-        lg: "inline-block",
-      }}
-      size={responsiveSize} 
-      color="blue.500" 
-      mr="2" >Log in</Button>
-      <Menu>
-        {/* Creation du menu burger avec props */}
-        <MenuButton 
-        size={responsiveSize}
-        as={IconButton}
-        aria-label="Options"
-        icon={<HamburgerIcon />}
-        color="blue.500"
-        />
-      </Menu>
-    </Box>
-  </Flex>
-)}
+      <Box p="2">
+        <Heading size={responsiveSize}><span role="img" aria-label="atom emoji">⚛</span> React-jobs.fr</Heading>
+      </Box>
+      <Spacer />
+      <Box>
+        <Button
+          display={{
+            sm: 'none',
+            md: 'inline-block',
+            lg: 'inline-block',
+          }}
+          size={responsiveSize}
+          color="blue.500"
+          variant="outline"
+          mr="2"
+        >
+          Sign Up
+        </Button>
+        <Button
+          display={{
+            sm: 'none',
+            md: 'inline-block',
+            lg: 'inline-block',
+          }}
+          size={responsiveSize}
+          color="blue.500"
+          mr="2"
+        >Log in
+        </Button>
+        <Menu>
+          {/* Creation du menu burger avec props */}
+          <MenuButton
+            size={responsiveSize}
+            as={IconButton}
+            aria-label="Options"
+            icon={<HamburgerIcon />}
+            color="blue.500"
+          />
+        </Menu>
+      </Box>
+    </Flex>
+  );
+};
 
 export default Header;
