48eb3f3801677ac56b609ec3cf9abfc414678d5a
AlexandreChochois <alexandre.chochois@gmail.com>
2021-07-24 10:56:12 +0200

    how to use an API example comment index of app.js

--- front-init/src/components/App/index.js
+++ front-init/src/components/App/index.js
@@ -50,3 +50,33 @@ export default function footer() {
   );
 }
 
+
+
+/* import {useState, useEffect} from 'react'
+
+function App() {
+
+  const [dataImg, setDataImg] = useState();
+
+  useEffect(() => {
+
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      setDataImg(data[0].url)
+    })
+  }, [])
+
+  return (
+    <div className="App">
+      {dataImg &&
+      <img src={dataImg}
+      alt="cat image" style={{width: "500px"}}
+      />}
+    </div>
+  );
+}
