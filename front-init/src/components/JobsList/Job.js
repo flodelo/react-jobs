cca9a0a4e7c4fb4bcef4f78f46225d939d591d6b
flodelo <florian.delobelle@gmail.com>
2021-07-28 20:27:36 +0200

    added styling accodion to job component

--- front-init/src/components/JobsList/Job.js
+++ front-init/src/components/JobsList/Job.js
@@ -3,38 +3,46 @@ import { Link } from 'react-router-dom';
 
 import {
   Heading, Box, Badge, Button, Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from '@chakra-ui/react';
 
 import PropTypes from 'prop-types';
 
 export default function Job({ id, intitule, description, typeContrat }) {
   return (
-    <Box bg="white" 
-    p={5} 
-    shadow="md" 
-    borderWidth="1px" 
-    w="90%" 
-    maxW="800px"
-    _hover={{
+    <Accordion width="80%" allowToggle _hover={{
       boxShadow: 'lg',
       borderLeftWidth: '2px',
-      borderLeftColor: 'blue.500',
-    }}
-    >
-    <Link to={`/jobs/pe/${id}`} key={id}>
+      borderLeftColor: 'blue.500'}}>
+    {/*  <Link to={`/jobs/pe/${id}`} key={id}> */}
       {/* <Heading as="h2" size="md">{jobs.description}</Heading>
       <Heading as="h3" size="sm" color="blue.500">{jobs.company}</Heading>
       <span>{jobs.technology.map((tech) => (<Badge colorScheme="gray" mr="1" key={tech}>{tech}</Badge>))}</span>
       ligne de code pour utiliser la méthode .map avec des composants UI
       <p>{jobs.locality}</p>
   <p>{jobs.contract}</p> */}
+  <AccordionItem>
+  <AccordionButton>
+  <Box flex="1" textAlign="left">
   <Heading as="h2" size="md">{intitule}</Heading>
-  <Badge >{typeContrat}</Badge>
-  <Heading as="h3" size="sm" color="blue.500">{id}</Heading>
-  <p as="h3" size="sm" color="blue.500">{description}</p>
-  {/* <p>{job.completed.toString()}</p> */}
-  </Link>
   </Box>
+  <Badge fontSize="1.25em" ml="1" colorScheme="green">{typeContrat}</Badge>
+  <AccordionIcon />
+  </AccordionButton>
+  <AccordionPanel>
+  <Heading as="h3" size="sm" color="blue.500">{id}</Heading>
+  <p as="h3">{description}</p>
+  <Button>Test</Button>
+  </AccordionPanel>
+  {/* <p>{job.completed.toString()}</p> */}
+  {/* </Link> */}
+  </AccordionItem>
+  
+  </Accordion>
   );
 }
 
