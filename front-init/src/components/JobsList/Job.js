893424c80af1f8951a88abd575afbd46d9dbc842
FlorianB-B <florian.brunbuisson@gmail.com>
2021-07-29 15:01:43 +0200

    Add external redirect links to Job

--- front-init/src/components/JobsList/Job.js
+++ front-init/src/components/JobsList/Job.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as ReactRouter } from 'react-router-dom';
+
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 import {
-  Heading, Box, Badge, Button, Text,
+  Heading, Box, Badge, Button, Text, Link,
   Accordion,
   AccordionItem,
   AccordionButton,
@@ -12,12 +14,12 @@ import {
 
 import PropTypes from 'prop-types';
 
-export default function Job({ id, intitule, description, typeContrat, entreprise }) {
+export default function Job({ val }) {
+
+  // console.log(val.origineOffre.partenaires[0].url)
+
   return (
-    <Accordion width="80%" allowToggle _hover={{
-      boxShadow: 'lg',
-      borderLeftWidth: '2px',
-      borderLeftColor: 'blue.500'}}>
+    <>
     {/*  <Link to={`/jobs/pe/${id}`} key={id}> */}
       {/* <Heading as="h2" size="md">{jobs.description}</Heading>
       <Heading as="h3" size="sm" color="blue.500">{jobs.company}</Heading>
@@ -25,28 +27,34 @@ export default function Job({ id, intitule, description, typeContrat, entreprise
       ligne de code pour utiliser la méthode .map avec des composants UI
       <p>{jobs.locality}</p>
   <p>{jobs.contract}</p> */}
-  <AccordionItem>
+  <AccordionItem _hover={{
+              boxShadow: 'lg',
+              borderLeftWidth: '2px',
+              borderLeftColor: 'blue.500'}}>
   <AccordionButton>
   <Box flex="1" textAlign="left">
-  <Heading as="h2" size="md">{intitule}</Heading>
+  <Heading as="h2" size="md">{val.intitule}</Heading>
   </Box>
-  <Badge fontSize="1.25em" ml="1" colorScheme="green">{typeContrat}</Badge>
+  <Badge fontSize="1.25em" ml="1" colorScheme="green">{val.typeContrat}</Badge>
   <AccordionIcon />
   </AccordionButton>
   <AccordionPanel>
-  <Heading as="h3" size="sm" color="blue.500">{entreprise}</Heading>
-  <p as="h3">{description}</p>
-  <Button>Test</Button>
+  <Heading as="h3" size="sm" color="blue.500">{val.entreprise.nom}</Heading>
+  <p as="h3">{val.description}</p>
+  <Link href={val.origineOffre.urlOrigine} isExternal><Button rightIcon={<ArrowForwardIcon />}>Je postule</Button></Link>
   </AccordionPanel>
   {/* <p>{job.completed.toString()}</p> */}
   {/* </Link> */}
   </AccordionItem>
-  
-  </Accordion>
+  </>
   );
 }
 
-{/* Job.propTypes = {
+{/*
+  
+  <Link href={val.origineOffre.partenaires[0].url == "undefined" ? val.origineOffre.urlOrigine : val.origineOffre.partenaires[0].url} isExternal>
+  
+   Job.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   company: PropTypes.string,
