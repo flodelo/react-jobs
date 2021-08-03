4b10fa7002bb8ba89625b0ffa8c1e763b78be45e
Bozo Le Clown <felana0695608707@gmail.com>
2021-08-03 12:53:58 +0200

    fixed bug into user model

--- back/app/models/user.js
+++ back/app/models/user.js
@@ -19,20 +19,11 @@ class User {
     // import the class of the error
 
 
-<<<<<<< HEAD
      constructor(data={}) {
          for (const prop in data) {
              this[prop] = data[prop];
          }
      }
-=======
-
-    constructor(data={}) {
-        for (const prop in data) {
-            this[prop] = data[prop];
-        }
-    }
->>>>>>> c9bd80d7dfd7e9a7f1925a9184fe597ad1d8925f
 
     // We are not sure yet to need this method
     // /**
