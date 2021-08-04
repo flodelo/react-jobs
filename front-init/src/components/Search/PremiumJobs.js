312b1d5e3d8f463d1fae92e7bd8dad017742126f
flodelo <florian.delobelle@gmail.com>
2021-08-04 16:16:51 +0200

    modified premium component

--- front-init/src/components/Search/PremiumJobs.js
+++ front-init/src/components/Search/PremiumJobs.js
@@ -54,20 +54,19 @@ export default function PremiumJobs({premiumJobs}) {
                       >
             <AccordionButton>
             <Box flex="1" textAlign="left">
-            <Heading as="h2" size={useBreakpointValue({ base: "xs", md: "sm" })}> {premiumJobs.title} FORMATEUR-DEVELOPPEUR REACT H/F
+            <Heading as="h2" size={useBreakpointValue({ base: "xs", md: "sm" })}>{premiumJobs.title}
             </Heading>
             </Box>
-            <Badge size={useBreakpointValue({ base: "xs", md: "sm" })} ml="1" colorScheme="">TELETRAVAIL</Badge>
+            <Badge size={useBreakpointValue({ base: "xs", md: "sm" })} ml="1" colorScheme="">{premiumJobs.locality}</Badge>
             <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
             <Flex justifyContent="space-between">
-            <Heading display="flex" as="h3" fontSize={useBreakpointValue({ base: "xs", md: "sm" })} color="blue.500">O'clock</Heading>
-            <Heading display="flex" as="h2" fontSize={useBreakpointValue({ base: "xs", md: "sm" })} color="blue.500">CDI</Heading>
+            <Heading display="flex" as="h3" fontSize={useBreakpointValue({ base: "xs", md: "sm" })} color="blue.500">{premiumJobs.locality}</Heading>
+            <Heading display="flex" as="h2" fontSize={useBreakpointValue({ base: "xs", md: "sm" })} color="blue.500">{premiumJobs.contract}</Heading>
             </Flex>
             <br />
-            <Text fontSize={useBreakpointValue({ base: "sm", md: "md" })} as="p">Notre méthode étant centrée sur l’humain, l’équipe pédagogique est la pierre angulaire de l’école. Tous ses membres doivent s’armer de patience et d’empathie, mais aussi de méthode, pour aider nos étudiants à découvrir le métier de développeur.
-            Chaque matin, les formateurs accueillent les étudiants au sein de la classe virtuelle du Cockpit, notre plateforme pédagogique. Les cours commencent à 9h et se terminent à 15h avec une heure de pause à midi, on n’est pas des tortionnaires. Après les cours, les formateurs donnent le challenge à la classe, et restent dans les parages pour aider chaque étudiant à s’attaquer au défi de la journée.
+            <Text fontSize={useBreakpointValue({ base: "sm", md: "md" })} as="p">{premiumJobs.description}
             </Text>
             <Text fontSize={useBreakpointValue({ base: "xs", md: "sm" })} fontWeight="700" as="p">Publié le : DATE</Text>
             <Link fontSize={useBreakpointValue({ base: "sm", md: "md" })} href="https://oclock.io/team" isExternal><Button rightIcon={<ArrowForwardIcon />}>Je postule</Button></Link>
